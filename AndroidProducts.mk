8fa5e9b4366cce46c8ef32ecbb113e9bfa52bf9c
Jamie Falconer <mrfluffyoven@gmail.com>
2024-09-24 11:14:28 +0100

    Codename vitamin

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_ossi.mk
+    $(LOCAL_DIR)/twrp_vitamin.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_ossi-user \
-    twrp_ossi-userdebug \
-    twrp_ossi-eng
+    twrp_vitamin-user \
+    twrp_vitamin-userdebug \
+    twrp_vitamin-eng
