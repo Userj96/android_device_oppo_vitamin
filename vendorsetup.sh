39128601059360f9114f5fe671a0d03091ea4eef
Jamie Falconer <mrfluffyoven@gmail.com>
2024-09-24 11:13:29 +0100

    Codename vitamin

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_ossi-user
-add_lunch_combo twrp_ossi-userdebug
-add_lunch_combo twrp_ossi-eng
+add_lunch_combo twrp_vitamin-user
+add_lunch_combo twrp_vitamin-userdebug
+add_lunch_combo twrp_vitamin-eng
